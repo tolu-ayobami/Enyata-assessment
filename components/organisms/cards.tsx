b273f7f9681035fafb7dd81b9564a771b5d94b92
Tolu Ayobami <toluneymar@gmail.com>
2025-10-07 09:26:10 +0100

    update

--- components/organisms/cards.tsx
+++ components/organisms/cards.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Cards: React.FC<{ label: string; count: number; icon: string }> = ({ label, count, icon }) => {
     return (
        
-            <div className="w-[208px] h-[130px] max-md:w-[90%] px-4 py-4 flex flex-col justify-between dark:bg-gray-900 shadow-soft bg-white rounded-lg dark:border dark:border-gray-700">
+            <div className="w-[208px] h-[130px] max-md:w-[90%] px-4 py-4 flex flex-col justify-between dark:bg-gray-900 shadow-lg bg-white rounded-lg dark:border dark:border-gray-700">
                 <div className="flex justify-between items-center">
                     <p className="font-inter font-bold leading-[24px] text-[16px]">{label}</p>
                     <Image src={icon} alt={label} width={27} height={26} />
